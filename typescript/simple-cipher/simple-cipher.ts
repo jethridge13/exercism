ae8fdff425e88261deb43b4ee876b617e89065f7
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-09-30 20:34:11 -0400

    Update typescript: Simple Cipher based on mentor feedback

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -2,50 +2,51 @@ class SimpleCipher {
 
 	key:string;
 
-	constructor(key:string = 'null') {
-		// Need to include accept no argument but reject ''
-		// Therefore, using null as a keyword string
-		if (key.length === 0 ||
-			key.split('').some(element => {
-				return !isNaN(parseInt(element)) || /^[A-Z]+$/.test(element);
-			})) {
-			throw new Error('Bad key');
-		}
-		if (key === 'null') {
+    lowerCaseASCIIStartIndex = 97;
+    lowerCaseASCIIEndIndex = 122;
+    rangeOfValidCharacters = 26;
+
+	constructor(key:string = undefined) {
+		if (key === undefined) {
 			let keyArr = [];
 			while (keyArr.length < 100) {
-				let r = Math.floor(Math.random() * 26) + 97;
+				let r = Math.floor(Math.random() * this.rangeOfValidCharacters) + this.lowerCaseASCIIStartIndex;
 				keyArr.push(String.fromCharCode(r));
 			}
 			key = keyArr.join('');
 		}
+        if (key.length === 0 ||
+            key.split('').some(element => {
+                return !/^[a-z]+$/.test(element);
+            })) {
+            throw new Error('Bad key');
+        }
 		this.key = key;
 	}
 
-    encode(input: string):string {
+    _encodeAndDecodeHelper(input: string, method:string):string {
         let newInput = input.split('');
         newInput.forEach((char, index, arr) => {
-        	let charInt = char.charCodeAt(0);
-        	// Lowercase ASCII letters start at 97
-        	charInt += this.key[index % this.key.length].charCodeAt(0) - 97;
-        	if (charInt > 122) { charInt -= 123}
-        	charInt = 97 + (charInt % 97)
-        	arr[index] = String.fromCharCode(charInt);
+            let charInt = char.charCodeAt(0);
+            if (method.toLowerCase() === 'encode') {
+                charInt += this.key[index % this.key.length].charCodeAt(0) - this.lowerCaseASCIIStartIndex;
+            } else {
+                charInt -= this.key[index % this.key.length].charCodeAt(0) - this.lowerCaseASCIIStartIndex;
+            }
+            if (charInt > this.lowerCaseASCIIEndIndex) { charInt -= (this.lowerCaseASCIIEndIndex+1) }
+            else if (charInt < this.lowerCaseASCIIStartIndex) { charInt += (this.lowerCaseASCIIEndIndex+1) }
+            charInt = this.lowerCaseASCIIStartIndex + (charInt % this.lowerCaseASCIIStartIndex)
+            arr[index] = String.fromCharCode(charInt);
         });
         return newInput.join('');
     }
 
+    encode(input: string):string {
+        return this._encodeAndDecodeHelper(input, 'encode');
+    }
+
     decode(input: string):string {
-        let newInput = input.split('');
-        newInput.forEach((char, index, arr) => {
-        	let charInt = char.charCodeAt(0);
-        	// Lowercase ASCII letters start at 97
-        	charInt -= this.key[index % this.key.length].charCodeAt(0) - 97;
-        	if (charInt < 97) { charInt += 123 }
-        	charInt = 97 + (charInt % 97)
-        	arr[index] = String.fromCharCode(charInt);
-        });
-        return newInput.join('');
+        return this._encodeAndDecodeHelper(input, 'decode');
     }
 }
 
