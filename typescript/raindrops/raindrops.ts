7854e94ca19f7d17cb8cddcc91eb82ac4410dfc8
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-10-15 21:01:37 -0400

    Complete typescript: Raindrops

---
+++ typescript/raindrops/raindrops.ts
@@ -0,0 +1,20 @@
+export default class Raindrops {
+
+    convert(n: number): string {
+        let output = ''
+        if (n % 3 === 0) {
+            output += 'Pling'
+        }
+        if (n % 5 === 0) {
+            output += 'Plang'
+        }
+        if (n % 7 === 0) {
+            output += 'Plong'
+        }
+
+        if (output !== '') {
+            return output
+        }
+        return n.toString()
+    }
+}
